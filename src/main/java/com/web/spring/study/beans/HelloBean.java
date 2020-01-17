e04452b30a8ce63f871033b93077e71da3ba76bf
teacher <teacher@192.168.1.180>
2020-01-17 20:03:01 +0800

    Spring

---
+++ src/main/java/com/web/spring/study/beans/HelloBean.java
@@ -0,0 +1,14 @@
+package com.web.spring.study.beans;
+
+public class HelloBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/main/java/com/web/spring/study/beans/Test.java
@@ -0,0 +1,7 @@
+package com.web.spring.study.beans;
+
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
