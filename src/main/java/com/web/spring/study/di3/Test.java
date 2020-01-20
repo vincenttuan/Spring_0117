d809865f069832ea31e3187988326f4f4b1c48d0
teacher <teacher@192.168.1.180>
2020-01-20 20:27:12 +0800

    Spring

--- src/main/java/com/web/spring/study/di3/Test.java
+++ src/main/java/com/web/spring/study/di3/Test.java
@@ -8,6 +8,7 @@ public class Test {
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/java/com/web/spring/study/di3/applicationConfig.xml");
         UserDAO userDAO = (UserDAO)ctx.getBean("userDAO");
         System.out.println(userDAO);
+        System.out.println(userDAO.getUsers());
         
     }
 }
