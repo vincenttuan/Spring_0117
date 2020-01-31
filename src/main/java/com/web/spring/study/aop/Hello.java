617bfdbaba9eae4c47218fbe6302f0871ea3a732
teacher <teacher@192.168.1.180>
2020-01-31 19:39:38 +0800

    Spring

--- src/main/java/com/web/spring/study/aop/Hello.java
+++ src/main/java/com/web/spring/study/aop/Hello.java
@@ -7,10 +7,12 @@ public class Hello {
     private String name;
 
     public String getName() {
+        System.out.println("getName() 做完了");
         return "Hello, " + name;
     }
 
     public void setName(String name) {
+        System.out.println("setName() 做完了");
         this.name = name;
     }
     
--- src/main/java/com/web/spring/study/aop/HelloAspect.java
+++ src/main/java/com/web/spring/study/aop/HelloAspect.java
@@ -1,6 +1,7 @@
 package com.web.spring.study.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,13 @@ public class HelloAspect {
     
     @Before(value = "execution(* com.web.spring.study.aop.Hello.setName(..))")
     public void before(JoinPoint jo) throws Throwable {
-        System.out.print("AOP 攔截 ");
+        System.out.print("AOP Before 攔截 ");
+        System.out.println(jo.getTarget().getClass().getName() + "." + jo.getSignature().getName());
+    }
+    
+    @After(value = "execution(* com.web.spring.study.aop.Hello.getName(..))")
+    public void after(JoinPoint jo) throws Throwable {
+        System.out.print("AOP After 攔截 ");
         System.out.println(jo.getTarget().getClass().getName() + "." + jo.getSignature().getName());
     }
     
