648f2465a185c1d4d1ad850654394326229aefdf
teacher <teacher@192.168.1.180>
2020-02-03 19:00:45 +0800

    Spring

--- src/main/java/com/web/mvc/HelloController.java
+++ src/main/java/com/web/mvc/HelloController.java
@@ -3,6 +3,7 @@ package com.web.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/hello")
@@ -18,6 +19,14 @@ public class HelloController {
     @ResponseBody
     public String sayYes() {
         return "Say Yes";
+    }
+    
+    @RequestMapping("/page")
+    public ModelAndView callHelloJspPage() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", "John");
+        mv.setViewName("/WEB-INF/jsp/hello.jsp");
+        return mv;
     }
     
 }
