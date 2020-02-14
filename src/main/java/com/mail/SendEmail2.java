e53ef1e6b7c52166fc1a66963ca88960062cb5b1
teacher <teacher@192.168.1.180>
2020-02-14 21:28:12 +0800

    Spring

--- src/main/java/com/mail/SendEmail2.java
+++ src/main/java/com/mail/SendEmail2.java
@@ -7,6 +7,7 @@ public class SendEmail2 {
     public static void main(String[] args) throws Exception {
         File file1 = new File("src\\main\\java\\com\\mail\\content.html");
         String content = new Scanner(file1, "UTF-8").useDelimiter("\\A").next();
+        content = String.format(content, "親愛的");
         File file2 = new File("src\\main\\java\\com\\mail\\to.txt");
         String to = new Scanner(file2, "UTF-8").useDelimiter("\\A").next();
         System.out.println(content);
