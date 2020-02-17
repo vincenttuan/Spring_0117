01c03c3490a734c397fdf2678054d04f428b4cb6
teacher <teacher@192.168.1.180>
2020-02-17 19:51:38 +0800

    Spring

---
+++ src/main/java/com/web/jdbc/member/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.web.jdbc.member.controller;
+
+import com.web.jdbc.member.beans.Member;
+import com.web.jdbc.member.repository.MemberDao;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * /jdbc/member/input 
+ * /jdbc/member/query 
+ * /jdbc/member/get/{id}
+ * /jdbc/member/update/{id} 
+ * /jdbc/member/delete/{id}
+ */
+@Controller
+@RequestMapping("/jdbc/member")
+public class MemberController {
+
+    @Autowired
+    private MemberDao dao;
+    
+    @RequestMapping("/save")
+    @ResponseBody
+    public String register(@ModelAttribute Member member) {
+        String code = Integer.toHexString(member.hashCode());
+        member.setCode(code);
+        member.setPass(Boolean.FALSE);
+        member.setPriority(1);
+        dao.save(member);
+        return member + " saved.";
+    }
+}
