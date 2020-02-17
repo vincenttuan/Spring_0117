f3d82f89f19aba7ad9ebd98427837687bcd0662e
teacher <teacher@192.168.1.180>
2020-02-17 20:08:32 +0800

    Spring

--- src/main/java/com/web/jdbc/member/repository/MemberDaoImpl.java
+++ src/main/java/com/web/jdbc/member/repository/MemberDaoImpl.java
@@ -3,6 +3,7 @@ package com.web.jdbc.member.repository;
 import com.web.jdbc.member.beans.Member;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,17 +14,27 @@ public class MemberDaoImpl implements MemberDao {
     
     @Override
     public List<Member> query() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM MEMBER";
+        List<Member> list = jdbcTemplate.query(sql, new BeanPropertyRowMapper<Member>(Member.class));
+        return list;
     }
 
     @Override
     public Member getMember(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM MEMBER WHERE ID = ?";
+        Member member = jdbcTemplate.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<Member>(Member.class));
+        return member;
     }
 
     @Override
     public String getUsername(String username) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT USERNAME FROM MEMBER WHERE USERNAME = ?";
+        String name = null;
+        try {
+            name = jdbcTemplate.queryForObject(sql, new Object[]{username}, String.class);
+        } catch (Exception e) {
+        }
+        return name;
     }
 
     @Override
@@ -50,12 +61,16 @@ public class MemberDaoImpl implements MemberDao {
 
     @Override
     public Boolean update(Member member) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "UPDATE MEMBER SET PASSWORD = ?, EMAIL = ?, PASS = ?, PRIORITY = ? WHERE ID = ?";
+        int rowCount = jdbcTemplate.update(sql, member.getPassword(), member.getEmail(), member.getPass(), member.getPriority(), member.getId());
+        return rowCount == 1 ? true : false;
     }
 
     @Override
     public Boolean delete(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "DELETE FROM MEMBER WHERE ID = ?";
+        int rowCount = jdbcTemplate.update(sql, id);
+        return rowCount == 1 ? true : false;
     }
     
 }
